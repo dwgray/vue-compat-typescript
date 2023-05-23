9344360b403a84565a5bb60a1d2bc86adfcc2df1
David W. Gray <dwgray67@hotmail.com>
2023-05-23 13:39:53 -0700

    Add explicit alias from vue$ to a file

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
+const path = require("path");
+
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: (config) => {
-    config.resolve.alias.set("vue", "@vue/compat");
+    config.resolve.alias.set(
+      "vue$",
+      path.resolve(
+        __dirname,
+        "node_modules/@vue/compat/dist/vue.runtime.esm-bundler.js"
+      )
+    );
 
     config.module
       .rule("vue")
